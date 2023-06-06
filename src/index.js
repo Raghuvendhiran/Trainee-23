c396ff369f43370c3a488cdd9a448465ab8a8531
Raghuvendhiran <raghuvendhiran@gmail.com>
2023-06-06 20:25:53 +0530

    shop

---
+++ src/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import ShopStyle from './/Tasks/index';
+
+
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <ShopStyle />
+  </React.StrictMode>
+);
+
